c1d239adaceb8fa63ebb0bb1621b7b9360b5addd
faroo <wizz.farooq@gmail.com>
2023-04-12 12:57:52 +0500

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Farooq sial'),
         ),
         body: Center(
           child: Container(
