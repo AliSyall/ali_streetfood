ba838a65ab35eeeb53981d29b29c16194677521f
wizza <wizz.ali>
2023-04-18 11:12:05 +0500

    five commmit

--- lib/Screen_signup.dart
+++ lib/Screen_signup.dart
@@ -22,42 +22,111 @@ class Signup extends StatelessWidget {
               Stack(
                 children: [
                   Positioned(
-                    left: 50,
+                    left: 40,
                     child: Container(
-                      width: 83,
-                      height: 83,
-                      decoration: BoxDecoration(
-                          color: Colors.yellow, shape: BoxShape.circle),
+                      width: 100,
+                      height: 100,
+                      decoration:
+                      BoxDecoration(color: Colors.yellow, shape: BoxShape.circle),
                     ),
                   ),
                   Positioned(
-                    left: 87,
-                    top: 25,
+                    left: 90,
+                    top: 33,
                     child: Container(
-                      child: Text(
-                        'Create a New Account',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 21,
-                          fontFamily: 'Poppinsr',
-                        ),
+                      child: Text('Create a New Account',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                        fontSize: 21,
+                        fontFamily: 'Poppinsr',
+                      ),
                       ),
                     ),
                   ),
                 ],
               ),
-              Text(
-                'Create an account so you can see  our\n latest Racipes',
-                style: TextStyle(
-                  color: Color(0xffADAAAA),
-                  fontFamily: 'Poppinsr',
-                  fontSize: 12,
-                ),
-              ),
-              TextField()
+              Text('Create an account so you can see  our '),
             ],
           ),
         ),
+        // body: Center(
+        //   child: Column(
+        //     children: [
+        //       Stack(
+        //         children: [
+        //           Positioned(
+        //             left: 40,
+        //             child: Container(
+        //               width: 100,
+        //               height: 100,
+        //               decoration: BoxDecoration(
+        //                 color: Colors.yellow,
+        //                 shape: BoxShape.circle,
+        //               ),
+        //             ),
+        //           ),
+        //           Positioned(
+        //             top: 33,
+        //             left: 90,
+        //             child: Container(
+        //               child: Text('Create a New Account',
+        //               style: TextStyle(
+        //                 fontWeight: FontWeight.w600,
+        //                 color: Color(0xff000000),
+        //                 fontSize: 21,
+        //                 fontFamily: 'Poppinsr',
+        //               ),
+        //               ),
+        //               ),
+        //             ),
+        //         ],
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        // body: Center(
+        //   child: Column(
+        //     children: [
+        //       Stack(
+        //         children: [
+        //           Positioned(
+        //             left: 50,
+        //             child: Container(
+        //               width: 83,
+        //               height: 83,
+        //               decoration: BoxDecoration(
+        //                   color: Colors.yellow, shape: BoxShape.circle),
+        //             ),
+        //           ),
+        //           Positioned(
+        //             left: 87,
+        //             top: 25,
+        //             child: Container(
+        //               child: Text(
+        //                 'Create a New Account',
+        //                 style: TextStyle(
+        //                   color: Colors.black,
+        //                   fontSize: 21,
+        //                   fontFamily: 'Poppinsr',
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //       Text(
+        //         'Create an account so you can see  our\n latest Racipes',
+        //         style: TextStyle(
+        //           color: Color(0xffADAAAA),
+        //           fontFamily: 'Poppinsr',
+        //           fontSize: 12,
+        //         ),
+        //       ),
+        //       TextField()
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
